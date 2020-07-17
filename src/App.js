1b027d8a041a916792e6f363a7a6f87ed9fe2259
Emerson Silva <emerson_jdss@hotmail.com>
2020-07-17 15:35:15 -0300

    add test suit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./styles.css";
 
 function App() {
+  async function handleAddRepository() {
+    // TODO
+  }
+
+  async function handleRemoveRepository(id) {
+    // TODO
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul data-testid="repository-list">
+        <li>
+          Repositório 1
+
+          <button onClick={() => handleRemoveRepository(1)}>
+            Remover
+          </button>
+        </li>
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
