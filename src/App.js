3853edb4acc07aec8ddee3bc614eaccfdec00548
Emerson Silva <emerson_jdss@hotmail.com>
2020-07-20 16:28:09 -0300

    ternary for validation of repository

--- src/App.js
+++ src/App.js
@@ -4,27 +4,27 @@ import api from "./services/api";
 import "./styles.css";
 
 function App() {
-  const { repositories, setRepositories } = useState([
-    "Emerson",
-    "Maria",
-    "jose",
-  ]);
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    handleAddRepository();
-    handleAddRepository();
+    // handleAddRepository();
+    api.get("/repositories").then((response) => {
+      setProjects([...projects, response.data]);
+      // setRepositories([...repositories, response.data]);
+    });
   }, []);
 
   async function handleAddRepository() {
     //TODO: change for POST method
     const response = await api.post("/repositories", {
       url: "github.com/emersonjds",
-      title: "React JS",
+      title: "Desafio ReactJS",
       techs: ["React JS", "React Native"],
     });
 
     const { data } = response.data;
-    // setRepositories(...repositories, data);
+    setProjects([...projects, data]);
+    console.log(projects);
   }
 
   async function handleRemoveRepository(id) {
@@ -34,13 +34,19 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>{repositories}</li>
-        {/* {repositories.map((repository) => (
-          <li key={repository.id}>
-            {repository.title}
-            <button onClick={() => handleRemoveRepository(1)}>Remover</button>
-          </li>
-        ))} */}
+        {projects &&
+          projects.map((project) => (
+            <li key={project.id}>
+              {project.title}
+              {project.id ? (
+                <button onClick={() => handleRemoveRepository(1)}>
+                  Remover
+                </button>
+              ) : (
+                ""
+              )}
+            </li>
+          ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
